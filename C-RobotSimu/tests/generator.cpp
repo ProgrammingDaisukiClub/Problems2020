faa6f7c75e7bd0182a570c7303ad1720109176c5
reiya <t-reiya@suou.waseda.jp>
2020-05-04 15:46:22 +0900

    Modify generator.cpp & PROBLEM

--- C-RobotSimu/tests/generator.cpp
+++ C-RobotSimu/tests/generator.cpp
@@ -21,8 +21,7 @@ void random(string filename, int T, int min_N, int max_N, int min_X, int max_X){
 
 signed main(int argc, char* argv[]){
     registerGen(argc, argv, 1);
-    random("small.in", 5, MIN_N, 10, -10, 10);
-    random("medium.in", 50, MIN_N, MAX_N, MIN_X, MAX_X);
+    random("small.in", 5, MIN_N, 10, 1, 10);
     random("large.in", 100, MIN_N, MAX_N, MIN_X, MAX_X);
     for(int i=0;i<10;++i){
         random("random-test" + to_string(i) + ".in", 100, MIN_N, MAX_N, MIN_X, MAX_X);
