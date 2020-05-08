ed82f4182d58b5f2a85df311affd33b615dd926d
DAyamaCTF <taiki_yamada@akane.waseda.jp>
2020-05-08 17:05:26 +0900

    a

--- G-mojiretsu/C-yamada-AC/main.c
+++ G-mojiretsu/C-yamada-AC/main.c
@@ -11,22 +11,12 @@ int main() {
     char S[N + 1];
     scanf("%s", S);
 
-<<<<<<< HEAD
-        int found = 0;
-        for(int i = 0; i < N - 3; i++) {
-            if(S[i] == '2' && S[i + 1] == '5' && S[i + 2] == '2' && S[i + 3] == '0') {
-                found = 1;
-            }
-        }
-=======
     int found = 0;
-    for (int i = 0; i < N - 3; i++) {
-      if (S[i] == '2' && S[i + 1] == '5' && S[i + 1] == '5' &&
-          S[i + 1] == '5') {
+    for(int i = 0; i < N - 3; i++) {
+      if(S[i] == '2' && S[i + 1] == '5' && S[i + 2] == '2' && S[i + 3] == '0') {
         found = 1;
       }
     }
->>>>>>> 1703925d83e19ca19f1cb09ad93e145fad0d44e8
 
     if (found) {
       printf("Yes\n");
